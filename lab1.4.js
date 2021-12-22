c20996ffb34beae3619abcfb882a1f0a2175b8f8
Ivan Shevtsov <62093802+ivanShevtsov@users.noreply.github.com>
2021-12-22 03:50:41 +0200

    Create lab1.4.js

---
+++ lab1.4.js
@@ -0,0 +1,6 @@
+var createCustomerID = function (name, id) { return name + " " + id; };
+var myID = createCustomerID("Ann", 10);
+console.log(myID);
+var idGenerator;
+idGenerator = createCustomerID;
+console.log(idGenerator("Ivan", 85));
