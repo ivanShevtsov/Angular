89f7da2ab7038cd805b4a9518ab7b0e16a74d3fe
Ivan Shevtsov <62093802+ivanShevtsov@users.noreply.github.com>
2021-12-22 03:50:59 +0200

    Create lab2.4.js

---
+++ lab2.4.js
@@ -0,0 +1,11 @@
+var UniversityLibrarian = /** @class */ (function () {
+    function UniversityLibrarian() {
+    }
+    UniversityLibrarian.prototype.assistCustomer = function (custName) {
+        console.log(this.name + " is assisting " + custName);
+    };
+    return UniversityLibrarian;
+}());
+var librarian = new UniversityLibrarian();
+librarian.name = "Librarian1";
+librarian.assistCustomer("Customer");
